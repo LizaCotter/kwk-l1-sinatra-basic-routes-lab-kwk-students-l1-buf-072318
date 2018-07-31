6463eb94f8da3a522db3134f02d4f264d6b77dc6
LizaCotter <ecotter20@allendalecolumbia.org>
2018-07-31 20:44:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ class App < Sinatra::Base
   get '/' do
     "Hello, World!"
   end
+<<<<<<< HEAD
   get '/name' do
     "My name is Peyton"
   end 
@@ -13,4 +14,6 @@ class App < Sinatra::Base
   get '/favorite-song' do
     "My favorite song is You Belong With Me"
   end
+=======
+>>>>>>> 5b7195d1a8cbb5c4a53e05f59f494570ffe2f3a4
 end
